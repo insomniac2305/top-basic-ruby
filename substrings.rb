3a3ccf5936325a369767a1eead5c8f088ddde9a0
Philipp <p.doerner@outlook.com>
2021-07-02 16:41:33 +0200

    Implement Substrings Method

---
+++ substrings.rb
@@ -0,0 +1,17 @@
+def substrings(sentence, dictionary)
+  result = Hash.new(0)
+
+  dictionary.each do |entry| 
+    working_string = sentence.upcase  
+    while index_of_entry = working_string.index(entry.upcase) do
+      result[entry] += 1
+      working_string = working_string[index_of_entry+entry.length..working_string.length]
+    end
+  end
+  
+  return result
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
