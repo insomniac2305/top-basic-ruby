47a7e0fe30103ac2ad1f8a027cd17f17ec38adb3
Philipp <p.doerner@outlook.com>
2021-07-02 18:08:02 +0200

    Implement Bubble Sort

---
+++ bubble_sort.rb
@@ -0,0 +1,26 @@
+def bubble_sort(array)
+
+  is_sorted = false
+  iteration = 1
+
+  until is_sorted
+
+    is_sorted = true
+
+    for index in 1..array.length-iteration do
+      a = array[index-1]
+      b = array[index]
+      if a > b
+        array[index-1] = b
+        array[index] = a
+        is_sorted = false
+      end
+    end    
+
+  end
+
+  return array
+  
+end
+
+p bubble_sort([4,3,78,2,0,2])
